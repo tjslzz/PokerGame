5a916f3ab9669872c94989cfedf95462a929ef17
GalaxyXie <771472141@qq.com>
2019-08-17 16:46:20 +0800

    [Laura: finished test should_return_player2_when_1H_3H_5S_6S_QD_VS_1H_3H_5S_JS_QD]

---
+++ src/main/java/PokerGame.java
@@ -0,0 +1,5 @@
+public class PokerGame {
+    public String getWinner(String player1, String player2) {
+        return "";
+    }
+}
---
+++ src/test/java/PokerGameTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PokerGameTest {
+    @Test
+    public void should_return_player2_when_1H_3H_5S_6S_QD_VS_1H_3H_5S_JS_QD(){
+        PokerGame pokerGame=new PokerGame();
+        String player1 = "1H 3H 5S 6S QD";
+        String player2 = "1H 3H 5S JS QD";
+        String result = pokerGame.getWinner(player1, player2);
+        assertEquals("Winner:player2",result);
+
+    }
+}
